31560b2bf3d9859bf4092f452d9c2fc37e3b6868
yjksw <cairofaith@gmail.com>
2021-05-10 17:18:36 +0900

    feat: 노선 조회 추가 기능 구현
    - 정렬된 상행역 -> 하행역 목록 반환

--- src/main/java/wooteco/subway/station/StationResponse.java
+++ src/main/java/wooteco/subway/station/StationResponse.java
@@ -7,9 +7,14 @@ public class StationResponse {
     public StationResponse() {
     }
 
-    public StationResponse(Long id, String name) {
+    public StationResponse(Long id, String name) {//TODO 삭제
         this.id = id;
         this.name = name;
+    }
+
+    public StationResponse(Station station) {
+        this.id = station.getId();
+        this.name = station.getName();
     }
 
     public Long getId() {
